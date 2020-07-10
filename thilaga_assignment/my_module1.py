16bc42119cfd562bd569a3b445935eff97ab80f2
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-07-10 15:48:23 +0530

    modules are added

---
+++ thilaga_assignment/my_module1.py
@@ -0,0 +1,5 @@
+def name_func(name):
+    print("Hi,This is "+name)
+def value_func(a,b):
+    print('x:',a)
+    print('y:',b)
---
+++ thilaga_assignment/my_module2.py
@@ -0,0 +1,4 @@
+import my_module1
+
+my_module1.name_func("Bala")
+my_module1.value_func(10,20)
