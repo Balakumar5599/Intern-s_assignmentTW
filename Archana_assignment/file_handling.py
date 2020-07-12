28fbab572c282d731499769da13f5b17bb26926a
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-07-12 10:46:34 +0530

    archana assignment is added

---
+++ Archana_assignment/file_handling.py
@@ -0,0 +1,21 @@
+#File handling
+
+file=open("my_file.txt","w")
+file.write("My name is Balakumar M \nMy DOB is 05/05/1999\n")
+file.close()
+file=open("my_file.txt","a")
+file.write("My Degree is BE,ECE")
+file.close()
+file=open("my_file.txt","r")
+print(file.readline())
+print(file.readline())
+print(file.readline())
+
+#My output is look like
+'''
+My name is Balakumar M 
+
+My DOB is 05/05/1999
+
+My Degree is BE,ECE
+'''
