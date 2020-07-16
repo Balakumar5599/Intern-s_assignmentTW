4fc02b08d7548699f3d14b14566d3b659b64620a
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-07-16 13:09:55 +0530

    planet assignment is updated

--- planets_class.py
+++ planets_class.py
@@ -1,6 +1,6 @@
 # To find radius of the planet, number of days in planet, largest planet:-
 
-class Galaxy:
+class Planet:
 
     one_earth_year=365.25
     
@@ -10,34 +10,34 @@ class Galaxy:
         self.no_of_moons=no_of_moons
         self.length_year=length_year
         if "earth year" in self.length_year:
-            self.length_year=str(float(self.length_year.split()[0])*Galaxy.one_earth_year)
+            self.length_year=str(float(self.length_year.split()[0])*Planet.one_earth_year)
         else:
             self.length_year=self.length_year.split()[0]
+            
+    def planet_radius(self):
+        print("The Radius of planet Neptune is:",self.diameter/2,"km\n")
+        
+    def days_in_planet(self):
+        print("Number of days in Jupiter is:",round(float(self.length_year)),"days\n")
 
-
-def rad_of_neptune():
-    print("The Radius of planet Neptune is:",neptune.diameter/2,"km\n")
-
-def days_in_jupiter():
-    print("Number of days in Jupiter is:",round(float(jupiter.length_year)),"days\n")
 
 def largest_planet():
-    l=[]
-    x=[mercury,venus,earth,mars,jupiter,saturn,uranus,neptune]
-    for i in range(len(x)):
-        l.append(x[i].diameter)
-    print("The largest planet is:",x[l.index(max(l))].planet_name)
+    list=[]
+    obj_list=[mercury,venus,earth,mars,jupiter,saturn,uranus,neptune]
+    for planet_obj in range(len(obj_list)):
+        list.append(obj_list[planet_obj].diameter)
+    print("The largest planet is:",obj_list[list.index(max(list))].planet_name)
         
-mercury = Galaxy("Mercury",4879,0,"88 days")
-venus = Galaxy("Venus",12100,0,"225 days")
-earth = Galaxy("Earth",12755,1,"365 days")
-mars = Galaxy("Mars",6786,2,"687 days")
-jupiter = Galaxy("Jupiter",142800,79,"12 earth year")
-saturn = Galaxy("Saturn",120537,82,"29.5 earth year")
-uranus = Galaxy("Uranus",51819,27,"84 earth year")
-neptune = Galaxy("Neptune",49529,14,"165 earth year")
+mercury = Planet("Mercury",4879,0,"88 days")
+venus = Planet("Venus",12100,0,"225 days")
+earth = Planet("Earth",12755,1,"365 days")
+mars = Planet("Mars",6786,2,"687 days")
+jupiter = Planet("Jupiter",142800,79,"12 earth year")
+saturn = Planet("Saturn",120537,82,"29.5 earth year")
+uranus = Planet("Uranus",51819,27,"84 earth year")
+neptune = Planet("Neptune",49529,14,"165 earth year")
 
 
-rad_of_neptune()
-days_in_jupiter()
+neptune.planet_radius()
+jupiter.days_in_planet()
 largest_planet()
