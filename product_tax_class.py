c5ffd3baa5e60a1e3d10592c605a4744513ce823
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-07-17 14:39:23 +0530

    finding tax is updated with user input

--- product_tax_class.py
+++ product_tax_class.py
@@ -14,20 +14,8 @@ class Product:
         return self.tax_price
         
         
-cream=Product(101,"Ice cream",300,"dairy")
-pant=Product(102,"Pant",800,"textile")
-tomato=Product(103,"Tomato",150,"produce")
-table=Product(104,"Table",1400,"home needs")
-chocolate=Product(105,"Chocolate",1500,"dairy")
-potato=Product(106,"Potato",200,"produce")
-shirt=Product(107,"Shirt",600,"textile")
-chair=Product(108,"Chair",900,"home needs")
-milk=Product(109,"Milk",100,"dairy")
-saree=Product(110,"Saree",2000,"textile")
-
 def total_tax():
     
-    product_list=[cream,pant,tomato,table,chocolate,potato,shirt,chair,milk,saree]
     overall_bill=0
     overall_tax=0
     
@@ -48,25 +36,105 @@ def total_tax():
         
     print()    
     print("Overall Tax Amount: Rs.",overall_tax,"\n")
-    print("Overall Bill Amount: Rs.",overall_bill)
-    
+    print("Overall Bill Amount: Rs.(Incl.of all taxes)",overall_bill)
+
+print("Note:\nThe category of products can be only one of these following: Dairy, Textile, Produce, Homeneeds\n")
+num_of_product=int(input("Enter the number of products: "))
+product_list=[]
+for items_no in range(num_of_product):
+    print()
+    print("Enter the details of product",items_no+1,":-")
+    pid=int(input("Enter the product ID: "))
+    name=input("Enter the product name: ")
+    obj_name=name.lower().replace(" ","")
+    price=float(input("Enter the product price: "))
+    category=input("Enter the product category: ").lower()
+    obj_name=Product(pid,name,price,category)
+    product_list.append(obj_name)
+print()
+
 total_tax()
 
 '''
 My output:-
 
-Tax amount of Ice cream: Rs.6.0 --->Total price of Ice cream: Rs. 306.0
-Tax amount of Pant: Rs.48.0 --->Total price of Pant: Rs. 848.0
-Tax amount of Tomato: Rs.3.0 --->Total price of Tomato: Rs. 153.0
-Tax amount of Table: Rs.70.0 --->Total price of Table: Rs. 1470.0
-Tax amount of Chocolate: Rs.120.0 --->Total price of Chocolate: Rs. 1620.0
-Tax amount of Potato: Rs.4.0 --->Total price of Potato: Rs. 204.0
-Tax amount of Shirt: Rs.36.0 --->Total price of Shirt: Rs. 636.0
-Tax amount of Chair: Rs.45.0 --->Total price of Chair: Rs. 945.0
-Tax amount of Milk: Rs.2.0 --->Total price of Milk: Rs. 102.0
-Tax amount of Saree: Rs.120.0 --->Total price of Saree: Rs. 2120.0
+Note:
+The category of products can be only one of these following: Dairy, Textile, Produce, Homeneeds
+
+Enter the number of products: 10
+
+Enter the details of product 1 :-
+Enter the product ID: 101
+Enter the product name: icecream
+Enter the product price: 300
+Enter the product category: dairy
+
+Enter the details of product 2 :-
+Enter the product ID: 102
+Enter the product name: pant
+Enter the product price: 800
+Enter the product category: textile
+
+Enter the details of product 3 :-
+Enter the product ID: 103
+Enter the product name: tomato
+Enter the product price: 150
+Enter the product category: produce
+
+Enter the details of product 4 :-
+Enter the product ID: 104
+Enter the product name: table
+Enter the product price: 1400
+Enter the product category: home needs
+
+Enter the details of product 5 :-
+Enter the product ID: 105
+Enter the product name: chocolate
+Enter the product price: 1500
+Enter the product category: dairy
+
+Enter the details of product 6 :-
+Enter the product ID: 106
+Enter the product name: potato
+Enter the product price: 200
+Enter the product category: produce
+
+Enter the details of product 7 :-
+Enter the product ID: 107
+Enter the product name: shirt
+Enter the product price: 600
+Enter the product category: textile
+
+Enter the details of product 8 :-
+Enter the product ID: 108
+Enter the product name: chair
+Enter the product price: 900
+Enter the product category: home needs
+
+Enter the details of product 9 :-
+Enter the product ID: 109
+Enter the product name: milk
+Enter the product price: 100
+Enter the product category: dairy
+
+Enter the details of product 10 :-
+Enter the product ID: 110
+Enter the product name: saree
+Enter the product price: 2000
+Enter the product category: textile
+
+Tax amount of icecream: Rs.6.0 --->Total price of icecream: Rs. 306.0
+Tax amount of pant: Rs.48.0 --->Total price of pant: Rs. 848.0
+Tax amount of tomato: Rs.3.0 --->Total price of tomato: Rs. 153.0
+Tax amount of table: Rs.70.0 --->Total price of table: Rs. 1470.0
+Tax amount of chocolate: Rs.120.0 --->Total price of chocolate: Rs. 1620.0
+Tax amount of potato: Rs.4.0 --->Total price of potato: Rs. 204.0
+Tax amount of shirt: Rs.36.0 --->Total price of shirt: Rs. 636.0
+Tax amount of chair: Rs.45.0 --->Total price of chair: Rs. 945.0
+Tax amount of milk: Rs.2.0 --->Total price of milk: Rs. 102.0
+Tax amount of saree: Rs.120.0 --->Total price of saree: Rs. 2120.0
 
 Overall Tax Amount: Rs. 454.0 
 
-Overall Bill Amount: Rs. 8404.0
+Overall Bill Amount: Rs.(Incl.of all taxes) 8404.0
 '''
