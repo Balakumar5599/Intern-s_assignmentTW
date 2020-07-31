2330f1bceaa33dbc626e3dd133dfe4f27a8b5b4f
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-07-31 19:42:05 +0530

    goride vehicle booking program is updated

--- updated_goride_vehicle_booking.py
+++ updated_goride_vehicle_booking.py
@@ -1,54 +1,58 @@
 class Vehicle:
 
-    def __init__(self,name,km_range,price,ac_or_not,max_people):
+    def __init__(self,name,km_range,price,ac_service,max_people):
 
         self.name=name
         self.km_range=km_range
         self.price=price
-        self.ac_or_not=ac_or_not
+        self.ac_service=ac_service
         self.max_people=max_people
 
     
 class GoRide:
 
-    def __init__(self,category,ac_or_not):
+    def __init__(self,category,ac_service):
         
         self.category=category
-        self.ac_or_not=ac_or_not
-        self.vehi_list=vehi_list
+        self.ac_service=ac_service
+        self.vehicle_list=vehicle_list
         
-    def check_category_and_print_max_people(self):
+    def check_category_and_ac_service(self,vehicle_list):
         
-        for vehicle in self.vehi_list:
-            if vehicle.name==self.category and vehicle.ac_or_not==self.ac_or_not:
+        for vehicle in self.vehicle_list:
+            if vehicle.name==self.category and vehicle.ac_service==self.ac_service:
                 print("\nNote: Maximum number of people allowed in "+self.category+":",vehicle.max_people)
                 return True
         else:
             return False
         
-    def display_price_menu(self,vehi_list):
+    def display_price_menu(self,vehicle_list):
         
-        if self.check_category_and_print_max_people()==True:
+        if self.check_category_and_ac_service(vehicle_list)==True:
             print("\n*******Menu for "+self.category+"*******\n")
             print("Category  Km range\tprice\n")
-            for vehicle in self.vehi_list:
-                if vehicle.name==self.category and vehicle.ac_or_not==self.ac_or_not:
+            for vehicle in self.vehicle_list:
+                if vehicle.name==self.category and vehicle.ac_service==self.ac_service:
                     print(vehicle.name,"\t",vehicle.km_range,"\tRs."+str(vehicle.price))
 
-            if self.category=="auto" and self.ac_or_not=="yes":
-                print("No Ac available for auto")
+        if self.category=="auto" and self.ac_service=="yes":
+            print("No Ac available for auto")
 
-    def book_or_not(self,vehi_list):
+    def do_u_want_to_book(self,vehicle_list):
         
-        for vehicle in self.vehi_list:
-            if (self.category==vehicle.name) and (self.ac_or_not==vehicle.ac_or_not):
-                return "*****Your ride is booked*****"
-        return "Please enter proper category or AC type"
+        if goride.check_category_and_ac_service(vehicle_list)==False:
+            print("Please enter proper category or Ac service")
+        else:
+            confirmation=input("\nDo you want to book ride?: ")
+            if confirmation=="yes":
+                print("\n*****Sucessfully booked in GoRide*****")
+            elif confirmation=="no":
+                print("\nYou are not booked in GoRide")
 
         
 
 category=input("Enter anyone of the category from[auto,micro,xl]: ").lower()
-ac_or_not=input("\nDo you want AC [type:yes/no]: ").lower()
+ac_service=input("\nDo you want AC [type:yes/no]: ").lower()
 
 auto1=Vehicle("auto","upto 15km",10,"no",3)
 auto2=Vehicle("auto","15-30km",8,"no",3)
@@ -62,11 +66,11 @@ xl2=Vehicle("xl","upto 15km",15,"no",10)
 xl3=Vehicle("xl","15 and above",14,"yes",10)
 xl4=Vehicle("xl","15 and above",15,"no",10)
 
-vehi_list=[auto1,auto2,auto3,micro1,micro2,micro3,micro4,xl1,xl2,xl3,xl4]
+vehicle_list=[auto1,auto2,auto3,micro1,micro2,micro3,micro4,xl1,xl2,xl3,xl4]
 
-goride=GoRide(category,ac_or_not)
-goride.display_price_menu(vehi_list)
-print("\n",goride.book_or_not(vehi_list))
+goride=GoRide(category,ac_service)
+goride.display_price_menu(vehicle_list)
+goride.do_u_want_to_book(vehicle_list)
 
 '''
 
@@ -85,6 +89,6 @@ Category  Km range	price
 micro 	 upto 15km 	Rs.12
 micro 	 15 and above 	Rs.10
 
- *****Your ride is booked*****
+ *****Sucessfully booked in GoRide*****
  
  '''
